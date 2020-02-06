4fe7a0bfd20ac852b2461bc3812c4c4e7947d24d
Sebastian Lozano <hairu.sebas@gmail.com>
2020-02-05 22:53:34 -0300

    Actualizacion a seccion de uso de arreglos anidados

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
 $name = "Hector Benitez";
 $jobs = [
 	[
-		'title' => 'PHP Developer'
+		'title' => 'PHP Developer',
+		'description' => 'This is an awesome job!!!'
 	],
-	1 => 'Python Dev',
-	2 => 'DevOps'
+	[
+		'title' => 'Python Dev'
+	],
+	[
+		'title' => 'DevOps'
+	]
 ];
 ?>
 <!doctype html>
@@ -56,8 +61,8 @@ $jobs = [
           <h3 class="border-bottom-gray" >Work Experience</h3>
           <ul>
             <li class="work-position">
-	    <h5><?php echo $jobs[0]; ?></h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
+	    <h5><?php echo $jobs[0]['title']; ?></h5>
+              <p><?php echo $jobs[0]['description']; ?></p>
               <strong>Achievements:</strong>
               <ul>
                 <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
@@ -66,7 +71,7 @@ $jobs = [
               </ul>
             </li>
             <li class="work-position">
-                <h5><?php echo $jobs[1]; ?></h5>
+                <h5><?php echo $jobs[1]['title']; ?></h5>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
                 <strong>Achievements:</strong>
                 <ul>
@@ -76,7 +81,7 @@ $jobs = [
                 </ul>
               </li>
               <li class="work-position">
-                  <h5><?php echo $jobs[2]; ?></h5>
+                  <h5><?php echo $jobs[2]['title']; ?></h5>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
                   <strong>Achievements:</strong>
                   <ul>
