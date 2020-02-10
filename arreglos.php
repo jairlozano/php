02667189053605bbe9bff7a6968599ac82a7ab6b
jairlozano <hairu.sebas@gmail.com>
2020-02-09 23:35:11 -0300

    Ejercicios de arreglos

--- arreglos.php
+++ arreglos.php
@@ -20,4 +20,40 @@ foreach(array_reverse($arreglo) as $value){
     echo $value.',';
 }
 echo '<br>¡Qué trabajo me ha costado!';
+echo '<br>---------------------------------------<br>';
+$paises = [
+    'Bolivia' => [
+        'Tarija',
+        'Sucre',
+        'Pando'
+    ],
+    'Chile' => [
+        'Santiago',
+        'Valdivia',
+        'Iquique'
+    ],
+    'Argentina' => [
+        'Tucuman',
+        'Salta',
+        'Oran'
+    ]
+];
+foreach($paises as $pais => $ciudad){
+    echo '<strong>'.$pais.':</strong> ';
+    foreach($ciudad as $dep){
+        echo ' '.$dep;
+    }
+    echo '<br>';
+}
+echo '---------------------------------------<br>';
+$valores = [23, 54, 32, 67, 34, 78, 98, 56, 21, 34, 57, 92, 12, 5, 61];
+sort($valores);
+for($i=0;$i<3;$i++){
+    echo $valores[$i].' ';
+}
+echo '<br>';
+rsort($valores);
+for($i=0;$i<3;$i++){
+    echo $valores[$i].' ';
+}
 ?>
